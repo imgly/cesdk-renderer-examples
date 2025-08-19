a199acc28ce5c4194116e68f4c9cd366dff487c2
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-08-19 16:05:26 +0200

    Update examples for v1.58.0

---
+++ demo_expressapi/src/app.mjs
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+import express from 'express';
+import multer from 'multer';
+import { execFile } from 'node:child_process';
+import fs from 'node:fs';
+import { promisify } from 'node:util';
+
+const processorPath =
+  process.env.IMGLY_PROCESSOR_PATH || '/opt/imgly-processor/imgly-processor';
+const processorPwd =
+  process.env.IMGLY_PROCESSOR_PATH || '/opt/imgly-processor/';
+
+const execFileAsync = promisify(execFile);
+const app = express();
+const port = 8080;
+const upload = multer({ dest: '/uploads/' });
+
+app.post('/export', upload.single('scene'), async (req, res) => {
+  const sceneFile = req.file;
+  const inputPath = `/uploads/${sceneFile.filename}`;
+  const outputPath = `/exports/${sceneFile.filename}.png`;
+
+  const { error, stdout, stderr } = await execFileAsync(
+    processorPath,
+    [
+      '--input',
+      inputPath,
+      '--output',
+      outputPath,
+      '--dpi',
+      '300',
+      // Allow CPU rendering for demonstration purposes
+      '--render-device',
+      'auto'
+    ],
+    { cwd: processorPwd }
+  );
+
+  if (error) {
+    console.error(error);
+    res.status(500).send(`Error processing scene file: ${stderr}`);
+    try {
+      fs.unlinkSync(inputPath);
+      fs.unlinkSync(outputPath);
+    } catch (error) {
+      console.error(`Error deleting files: ${error}`);
+    }
+    return;
+  }
+
+  try {
+    fs.unlinkSync(inputPath);
+  } catch (error) {
+    console.error(`Error deleting input file: ${error}`);
+  }
+  res.setHeader('Content-Type', 'image/png');
+  res.setHeader(
+    'Content-Disposition',
+    `inline; filename="${sceneFile.originalname.replace('.scene', '.png')}"`
+  );
+  res.sendFile(outputPath, (err) => {
+    try {
+      fs.unlinkSync(outputPath);
+    } catch (error) {
+      console.error(`Error deleting output file: ${error}`);
+    }
+  });
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`IMG.LY Processor Express API demo listening on port ${port}`);
+});
---
+++ demo_expressapi_variants/README.md
@@ -0,0 +1,9 @@
+# An Express.js API processing and exporting scene files on the server
+
+This demo combines the Node.js CreativeEditor SDK bindings with the IMG.LY Processor to modify a scene and then export it efficiently with GPU acceleration.
+
+## Running the sample
+
+1. Make sure you've set up [Docker](https://docs.docker.com/engine/install/) or a compatible container engine on your machine.
+2. Run `IMGLY_LICENSE=... docker compose up --build` to build and start the Express API gateway
+3. Connect to http://localhost:8080/ and upload a scene file in the HTML form to the export of multiple size variants of the scene and get the `.zip` of the variants back.
