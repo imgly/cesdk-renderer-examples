984d7ea5bd54d9d921e48d6e76b283ab57627e46
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-11-07 21:15:39 +0100

    Update examples for v1.64.0-rc.0

--- expressapi/src/app.mjs
+++ expressapi/src/app.mjs
@@ -1,25 +1,31 @@
 /* eslint-disable no-console */
+// highlight-imports
 import express from 'express';
 import multer from 'multer';
 import { execFile } from 'node:child_process';
 import fs from 'node:fs';
 import { promisify } from 'node:util';
+// highlight-imports
 
+// highlight-constants
 const rendererPath =
   process.env.CESDK_RENDERER_PATH || '/opt/cesdk-renderer/cesdk-renderer';
-const rendererPwd =
-  process.env.CESDK_RENDERER_PATH || '/opt/cesdk-renderer/';
+const rendererPwd = process.env.CESDK_RENDERER_PATH || '/opt/cesdk-renderer/';
 
 const execFileAsync = promisify(execFile);
 const app = express();
 const port = 8080;
 const upload = multer({ dest: '/uploads/' });
+// highlight-constants
 
+// highlight-post
 app.post('/export', upload.single('scene'), async (req, res) => {
   const sceneFile = req.file;
   const inputPath = `/uploads/${sceneFile.filename}`;
   const outputPath = `/exports/${sceneFile.filename}.png`;
+  // highlight-post
 
+  // highlight-exec
   const { error, stdout, stderr } = await execFileAsync(
     rendererPath,
     [
@@ -35,7 +41,9 @@ app.post('/export', upload.single('scene'), async (req, res) => {
     ],
     { cwd: rendererPwd }
   );
+  // highlight-exec
 
+  // highlight-error
   if (error) {
     console.error(error);
     res.status(500).send(`Error processing scene file: ${stderr}`);
@@ -47,7 +55,9 @@ app.post('/export', upload.single('scene'), async (req, res) => {
     }
     return;
   }
+  // highlight-error
 
+  // highlight-result
   try {
     fs.unlinkSync(inputPath);
   } catch (error) {
@@ -66,6 +76,7 @@ app.post('/export', upload.single('scene'), async (req, res) => {
     }
   });
 });
+// highlight-result
 
 app.use(express.static('public'));
 
